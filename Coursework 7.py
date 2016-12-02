3e27ff772346dcacc72d25b1582b803fa8c7b493
Daniel-Hebron <hebrond@uni.coventry.ac.uk>
2016-12-02 21:29:12 +0000

    Update Coursework 7.py

--- Coursework 7.py
+++ Coursework 7.py
@@ -1,10 +1,11 @@
 N = int(input('Enter A Number: '))
-if N > 1:
-    for i in range(2, N):
-        if(N % i) == 0:
+if N > 1: 
+    for i in range(2, N): #between 2 and the number user chose
+        if(N % i) == 0: #if N remainder equal to 0 it is prime
             print(N, 'is not a prime number')
             break
     else:
-        print(N, 'is not a prime number')
+        print(N, 'is a prime number')
 else:
-    print (N, 'is not a prime number')
+    print ('please input a positive number') #if user input negative number
+
