e9eae322aec2f36611cf33534cab0611c4a58a89
Daniel-Hebron <hebrond@uni.coventry.ac.uk>
2016-12-02 21:08:45 +0000

    Update Coursework 1.py

--- Coursework 1.py
+++ Coursework 1.py
@@ -1,2 +1,11 @@
 import random
-print (random.sample([5,3,8,6,1,9,2,7], 8))
+
+myList = [5,3,8,6,1,9,2,7] #list I will be shuffling
+shuffledList = [] # putting shuffled list into a new empty list
+
+while(myList!=[]):
+    n = random.randint(0, len(myList)-1)
+    shuffledList.append(myList[n]) #adding random integer from myList and deleting it from myList
+    del myList[n]
+
+print(shuffledList)
