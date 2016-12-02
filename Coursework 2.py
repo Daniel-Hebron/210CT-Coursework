5c2e2687412a620c7faac6843aa043a5abcb4c16
Daniel-Hebron <hebrond@uni.coventry.ac.uk>
2016-12-02 21:10:02 +0000

    Update Coursework 2.py

--- Coursework 2.py
+++ Coursework 2.py
@@ -1,8 +1,15 @@
-def TZ(n):
-    count = 0
-    while n>0:
-        n = n / 5
-        count = count + n
-    return int(count)
 n = int(input ('Factorial : '))
-print (TZ(n))
+factorial = 1
+trailingZeros = 0
+
+for i in range(1, n+1):  #starts at 1 and will end at users number + 1 
+    factorial = factorial * i
+
+while(True):
+    if int(factorial % 10) == 0: #'%' means it will show you the remainder from division
+        trailingZeros += 1 #if n is equal to 0, trailing zero = 1
+        factorial = factorial / 10
+    else:
+        break
+
+print(trailingZeros)
