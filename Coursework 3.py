8fbfa875c0b713f03f68a23f116d14cf910ea02c
Daniel-Hebron <hebrond@uni.coventry.ac.uk>
2016-12-02 21:10:36 +0000

    Update Coursework 3.py

--- Coursework 3.py
+++ Coursework 3.py
@@ -1,10 +1,14 @@
 s = (int(input('Input Number: ')))
 ans = 0
-if s >= 0:
+
+if s >= 0: 
     while ans*ans <= s:
         ans += 1
+        
     if ans*ans != s:
         ans=ans -1
         print('The Highest Square Number is', ans)
+        
     else: print('The Highest Square Number is', ans)
-else: print(s,'is an error')
+else: print(s,'is an error') 
+
